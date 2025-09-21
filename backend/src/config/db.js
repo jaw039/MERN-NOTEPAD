874c31ead0ac7f2cd0b2a734315f357ee878c6d9
jaw039 <jaw039@ucsd.edu>
2025-09-20 19:28:33 -0700

    move port to .env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const dbURI = process.env.MONGO_URI; 
-        await mongoose.connect(dbURI);
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MONGODB CONNECTED SUCCESSFULLY!");
     } catch (error) {
         console.error("Error Connecting to MONGODB", error);
+        process.exit(1)
     }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,16 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express(); 
+const PORT = process.env.PORT || 5001
 
 connectDB();
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(5001,() => {
-    console.log("Sever has started on PORT: 5001");
+app.listen(PORT,() => {
+    console.log("Sever has started on PORT: ", PORT);
 });
