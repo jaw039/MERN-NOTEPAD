3b41361846ff7fbb8146154b764e6260a570fc72
jaw039 <jaw039@ucsd.edu>
2025-09-25 13:00:58 -0700

    feat: fix noteboard ui

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -44,6 +44,8 @@ const HomePage = () => {
       <Navbar />
 
       {isRateLimited && <RateLimitedUi />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading  && <div className = "text-center text-primary py-10">Loading Notes....</div>}
         {notes.length > 0 && !isRateLimited && (
           <div className = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -54,7 +56,7 @@ const HomePage = () => {
           </div>
         )}
     </div>
-
+  </div>
   );
 };
  
