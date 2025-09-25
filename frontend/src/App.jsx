87ca363cc41205c884bd0e4b78f2819747bdad00
jaw039 <jaw039@ucsd.edu>
2025-09-25 13:13:10 -0700

    feat: implement CreatePage layout and state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import toast, { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <div data-theme = "nord">
-        {/* <button onClick = {() => toast.success("success")} className = "text-red-500 p-4 bg-pink-300">Click Me</button>  */}
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
         <Routes>
             <Route path = "/" element = {<HomePage />} />
             <Route path = "/create" element = {<CreatePage />} />
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { ArrowLeftIcon } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router";
 
 const CreatePage = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {
+
+  };
+
+
   return (
-    <div>CreatePage</div>
-  )
-}
+  <div className = "min-h-screen bg-base-200">
+    <div className = "container mx-auto px-4 py-8">
+      <div className = "max-w-2xl mx-auto">
+        <Link to = {"/"} className = "btn btn-ghost mb-6">
+          <ArrowLeftIcon className = "size-5" />
+          Back to All Notes
+        </Link>
+        </div> 
+    </div>
+  </div>
+  );
+};
 
 export default CreatePage;
