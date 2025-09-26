cf183258c5accc695160a7b5aa088bcfa4310d2d
jaw039 <jaw039@ucsd.edu>
2025-09-26 16:00:05 -0700

    feat: enhance NoteDetailPage with delete functionality and navigation improvements

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { useNavigate, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import api from "../libs/axios";
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from "lucide-react";
 
 
 const NoteDetailPage = () => {
@@ -16,8 +17,8 @@ const NoteDetailPage = () => {
   useEffect(() => {
     const fetchNote = async() => {
     try {
-      const res = await api.get(`/notes/${id}`)
-      setNote(res.data)
+      const res = await api.get(`/notes/${id}`);
+      setNote(res.data);
     } catch (error) {
       console.log("Error in fetching note", error);
       toast.error("Failed to fetch the note");
@@ -29,10 +30,53 @@ const NoteDetailPage = () => {
   fetchNote();
   }, [id]);
 
-  console.log({ note });
+  const handleDelete = () => {};
+
+  if(loading) {
+    return (
+      <div className ="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className = "animate-spin size-10" />
+      </div>
+    );
+  }
+  
   return (
-    <div>NoteDetailPage</div>
-  )
+    <div className = "min-h-screen bg-base-200">
+      <div className = "container mx-auto px-4 py-8">
+        <div className = "max-w-2xl mx-auto">
+          <div className = "flex items-center justify-between mb-6">
+            <Link to ="/" className = "btn btn-ghost">
+              <ArrowLeftIcon className = "h-5 w-5" />
+              Back to Notes
+            </Link>
+            <button onClick = {handleDelete} className = "btn btn-error btn-outline">
+              <Trash2Icon className = "h-5 w-5" />
+              Delete Note
+            </button>
+          </div>
+
+            <div className = "card bg-base-100">
+              <div className = "card-body">
+                <div className = "form-control mb-4">
+                  <label className = "label">
+                    <span className = "label-text"> 
+                      Title
+                    </span>
+                  </label>
+                  <input 
+                    type = "text"
+                    placeholder = "Note Title"
+                    className = "input input-bordered"
+                    value = {note.title}
+                    onChange = {(e) => setNote ({...note, title: e.target.value}) }
+                    />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 };
 
 export default NoteDetailPage;
