1ff9383bb946c2cab5088e8cd244590db3daa343
jaw039 <jaw039@ucsd.edu>
2025-09-26 17:01:30 -0700

    feat: update server configuration for production and enhance axios base URL handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,24 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 import cors from "cors"
 dotenv.config();
 
 const app = express(); 
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 
 // midddleware (runs before route handlers)
-app.use(cors({
-    origin: "http://localhost:5173",
-}))
+if (process.env.NODE_ENV != "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
 
 // convert incoming JSON requests into Javascript objecst on req.body
 app.use(express.json());
@@ -28,6 +34,16 @@ app.use((req,res,next) => {
 })
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    // call our frontend from the backend for deployment purposes 
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    // Catch-all handler for React Router (SPA)
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+    });
+}
 
 connectDB()
     .then(() => {
--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,9 +1,10 @@
 // Global Instance for our URL
-
 import axios from "axios"
 
+// there's no localhost in production so we have to make our links dynamic
+const BASE_URL = import .meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL : "http://localhost:5001/api"
-})
+    baseURL : BASE_URL,
+});
 
 export default api;
