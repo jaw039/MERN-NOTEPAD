72f995924e6663a39be28da08ae06765ca772874
jaw039 <jaw039@ucsd.edu>
2025-09-26 17:18:53 -0700

    Update server.js with working production configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,58 +1,45 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js"
+import cors from "cors";
 import dotenv from "dotenv";
-import rateLimiter from "./middleware/rateLimiter.js";
 import path from "path";
 
-import cors from "cors"
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
-const app = express(); 
-const PORT = process.env.PORT || 5001
-const __dirname = path.resolve()
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
-// midddleware (runs before route handlers)
-if (process.env.NODE_ENV != "production") {
-    app.use(
-        cors({
-            origin: "http://localhost:5173",
-        })
-    );
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
 
-// convert incoming JSON requests into Javascript objecst on req.body
-app.use(express.json());
-app.use(rateLimiter)
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
 
-// custom middleware 
-app.use((req,res,next) => {
-    console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-    next();
-})
 
 app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    // call our frontend from the backend for deployment purposes 
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    // Catch-all handler for React Router (SPA)
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-connectDB()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log("Server has started on PORT: ", PORT);
-        });
-    })
-    .catch((error) => {
-        console.error("Failed to connect to database:", error);
-        process.exit(1);
-    });
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
 
